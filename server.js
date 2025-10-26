51f14f2c05c936551c3c4e1cc281ec2837ee4c70
majidkambarath <majidmaji3410@gmail.com>
2025-10-26 20:40:17 +0400

    fix: Update CORS configuration to allow requests from the production domain

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const port = process.env.PORT || 4444;
 // Initialize Socket.IO with CORS
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173","https://venuegoldcrm.aurify.ae"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "x-secret-key", "Authorization"],
     credentials: true,
@@ -71,7 +71,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: ["http://localhost:5173","https://venuegoldcrm.aurify.ae"],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "x-secret-key", "Authorization"],
   credentials: true,
